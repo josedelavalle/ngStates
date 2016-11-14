85f07228a431e005e78a15ef4418c20c66b92254
Jose DeLavalle <me@slickwebstudio.com>
2016-11-13 19:19:36 -0500

    google maps

--- assets/js/app.js
+++ assets/js/app.js
@@ -26,12 +26,13 @@ app.controller("appController", ['$scope', 'getStates', function($scope, getStat
     });
 }]);
 
-app.controller("detailController", ['$scope', '$location', 'getStates', 'getStateData', function($scope, $location, getStates, getStateData) {
-
+app.controller("detailController", ['$scope', '$location', '$filter', 'getStates', 'getStateData', 'getImagery', function($scope, $location, $filter, getStates, getStateData, getImagery) {
+	$scope.imagery = [];
 	$scope.headerTitle = "Go Back";
   thisState = $location.search().s;
 
 	$scope.icons = ["fa-globe","fa-map","fa-flag","fa-map-marker","fa-road"];
+
 	switchState = function () {
 		x = thisState;
 		getStates.get().then(function (msg) {
@@ -79,6 +80,14 @@ app.controller("detailController", ['$scope', '$location', 'getStates', 'getStat
 	    });
 	};
 	switchState();
+	function formatDate(thisdate) {
+		return $filter('date')(thisdate,'yyyy-MM-dd');
+
+	}
+
+	$scope.getLocation = function(lat, lon, ndx) {
+		$scope.imagery[ndx] = "https://maps.googleapis.com/maps/api/staticmap?center="+lat+","+lon+"&zoom=11&size=400x400&key=AIzaSyBy34i8mK7IXxcAqmZfOEX70XZtNEt7D7s";
+	};
 }]);
 
 app.factory('getStates', function ($http) {
@@ -94,6 +103,19 @@ app.factory('getStateData', function ($http) {
     return {
         get: function (thisState) {
             return $http.get('./assets/json/' + thisState + '.json');
+        }
+    };
+});
+
+app.factory('getImagery', function ($http) {
+    return {
+        get: function (lat, lon, thisDate) {
+						//var thisURL = 'https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&date=' + thisDate + '&cloud_score=True&api_key=sFBD6hSaJ9HBP6U8qhXaBv6v9pKcTYtICStGJlOA';
+						var thisURL = "https://maps.googleapis.com/maps/api/staticmap?lon=58.76&lon=-156.83&zoom=13&size=600x300&maptype=roadmap&key=AIzaSyBy34i8mK7IXxcAqmZfOEX70XZtNEt7D7s";
+						var thisURL = "https://maps.googleapis.com/maps/api/streetview?size=600x300&location=46.414382,10.013988&heading=151.78&pitch=-0.76&key=AIzaSyBy34i8mK7IXxcAqmZfOEX70XZtNEt7D7s"
+						console.log(thisURL);
+            return $http.get(thisURL);
+						// return $http.get('https://api.nasa.gov/planetary/earth/imagery?lon=' + lon + '&lat=' + lat + '&date=2014-02-01&cloud_score=True&api_key=sFBD6hSaJ9HBP6U8qhXaBv6v9pKcTYtICStGJlOA');
         }
     };
 });
